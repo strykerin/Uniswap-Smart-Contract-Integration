b72af3888aede00774b87f109ed955f251821a25
Fabio Costa <dacosta.pereirafabio@gmail.com>
2021-01-18 23:00:12 -0300

    create two erc20 tokens

---
+++ contracts/erc20/FirstToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract FirstToken is ERC20 {
+    constructor() public ERC20("FirstToken", "FST"){
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
---
+++ contracts/erc20/SecondToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.6.6;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract SecondToken is ERC20 {
+    constructor() public ERC20("SecondToken", "SCN"){
+        _mint(msg.sender, 1000000000000000000000000);
+    }
+}
